75eeabe1b1159febf58ca054e85adfbba5eba7ee
Siarhei Voinich <svoinich@pa-magic-097.magicleap.ds>
2017-02-10 18:34:06 -0500

    Final touch on the stack

--- deploy_jenkins_slave_service.sh
+++ deploy_jenkins_slave_service.sh
@@ -3,5 +3,7 @@ docker service create --name jenkins-agent -e "SWARM_MASTER_URL=http://$(docker-
 -e "SWARM_JENKINS_USER=admin" \
 -e "SWARM_JENKINS_PASSWORD=admin" \
 blacklabelops/jenkins-swarm
-
-
+#echo "Scaling jenkins slaves to 5 nodes"
+#sleep 10
+#scale to 5 build slaves 
+#eval $(docker-machine env swarm-node-1) && docker service scale jenkins-agent=5
